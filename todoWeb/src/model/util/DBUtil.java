9aabc36b0b11f981f7527531097d85e6d8238d91
pabaep <sewon225@gmail.com>
2022-02-10 16:58:25 +0900

    [update] DB 연결 설정, 컨트롤러 파일 생성

---
+++ todoWeb/src/model/util/DBUtil.java
@@ -0,0 +1,67 @@
+package model.util;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DBUtil {
+	private static DataSource ds;
+	static {
+		
+		try {
+			Context initContext = new InitialContext();
+			Context envContext  = (Context)initContext.lookup("java:/comp/env");
+			ds = (DataSource)envContext.lookup("jdbc/myoracle");
+		}catch(NamingException e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		return ds.getConnection();
+	}
+	
+	//CP 기능의 close()는 CP메모리에 반환!!
+	public static void close(Connection con, Statement stmt, ResultSet rset) {
+		try {
+			if(rset != null) {
+				rset.close();
+				rset = null;
+			}
+			if(stmt != null) {
+				stmt.close();
+				stmt = null;
+			}
+			if(con != null) {
+				con.close();
+				con = null;
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Connection con, Statement stmt) {
+		try {
+			if(stmt != null) {
+				stmt.close();
+				stmt = null;
+			}
+			if(con != null) {
+				con.close();
+				con = null;
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
